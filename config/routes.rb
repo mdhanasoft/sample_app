6e1bb2b2a361ae51debff45a94e180b98a33ed5e
mdhanasoft <mdhanasoft@gmail.com>
2012-09-03 12:37:03 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "staticpages/home"
+  get "users/new"
 
-  get "staticpages/help"
-  get "/staticpages/about"
+  #get "staticpages/home"
 
+  #get "staticpages/help"
+  #get "/staticpages/about"
+  match '/signup', to: 'users#new'
+  match '/', to: 'staticpages#home'
+match '/help',    to: 'staticpages#help'
+  match '/about',   to: 'staticpages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,11 +58,11 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'staticpages#home'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
