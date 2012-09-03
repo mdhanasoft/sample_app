91e952b1b8cb2562f74955d4f00a3368f90907c3
mdhanasoft <mdhanasoft@gmail.com>
2012-09-03 11:04:23 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "staticpages/home"
 
   get "staticpages/help"
+  get "/staticpages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
